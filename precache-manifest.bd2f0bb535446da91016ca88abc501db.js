6b4de8aeabfe3844b731e7706ee52a96b528295f
Aftab <aftab.h.1993@gmail.com>
2020-07-26 00:27:01 +0530

    Updates

---
+++ precache-manifest.bd2f0bb535446da91016ca88abc501db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b2c7796bac4dfc0616eb4e642b8034",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "1c3c879e02a430509b9e",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1986fcf8411c2b332db0",
+    "url": "/facedetection/static/css/main.d3894c7b.chunk.css"
+  },
+  {
+    "revision": "1c3c879e02a430509b9e",
+    "url": "/facedetection/static/js/2.db4b7d3a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.db4b7d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1986fcf8411c2b332db0",
+    "url": "/facedetection/static/js/main.9273cbc9.chunk.js"
+  },
+  {
+    "revision": "fbb3e5bce481f305923d",
+    "url": "/facedetection/static/js/runtime-main.891b2e59.js"
+  }
+]);
